8a7eb502f3fa1eea17ad923f2c6a2cceafe73613
Peter Rufo <peter@peterrufo.com>
2019-11-12 11:30:10 -0500

    working on answers

--- index.js
+++ index.js
@@ -1,57 +1,40 @@
 'use strict';
 
 function loadStartPage() {
-  $('.container').html(
+  $('.quiz-area').html(
     `
-    <fieldset>
-      <div class="row">
-      <div class="col-12">
-        <p class="into">How much do you know about weather in the United States?</p>
-      </div>
-      </div>
-      <div class="row">
-      <div class="col-12">
-        <button type="button" id="startbutton"> Start Quiz</button>
-      </div>
-      </div>
-    </fieldset>
+     <p class="intro">How much do you know about weather in the United States?</p>      
+     
+     <div class="button">
+      <button type="button" id="startbutton"> Start Quiz</button>
+     </div>
    `
   );
-
+  
 }
 
 
-
-function resetScore() {
-  STORE.score = 0;
-  STORE.currentQuestion = 0;
-}
-function updateTicker() {
-  $('.ticker').html(
-    `
-    <span> Question: ${STORE.currentQuestion + 1} / ${STORE.quiz.length} </span>
-    <span> Score: ${STORE.score} / ${STORE.quiz.length} </span>
-    `
-  );
-}
-function startQuiz() {
-  resetScore();
-  updateTicker();
+function nextQuestion() {
   $('#startbutton').on('click', function(event){
     event.preventDefault();
-    renderQuestion();
+    if (STORE.currentQuestion < STORE.quiz.length){
+      STORE.currentQuestion++;
+      $('.quiz-area').html(renderQuestion());
+    }
+    else {
+      $('.quiz-area').html(renderFinalScore(STORE.score));
+    }
+
   });
-  
-  }
+}
 
 function renderQuestion() {
-   if (STORE.currentQuestion === STORE.quiz.length) {
-     renderFinalScore();
-   }
-   else{
-     let q = STORE.quiz[STORE.currentQuestion];
-     $('.container').html(
-       `
+  let q = STORE.quiz[STORE.currentQuestion -1];
+  $('.quiz-area').html(
+    `
+      <section class='tickers'>
+      <span class='question-ticker text>Question: ${STORE.currentQuestion}/${STORE.quiz.length}</span>
+      <span class='score-ticker text>Score: ${STORE.score}/${STORE.quiz.length}</span>
        <div>
        <form id="js-questions" class="question-form">
          <fieldset>
@@ -62,35 +45,73 @@ function renderQuestion() {
           <div class="answers">
           </div>
 
-          <div class="buttons">
-           <button type = "submit" id="answer" tabindex="5">Submit</button>
+          <div>
+           <button type = "submit" id="choice" tabindex="5"> Submit </button>
           </div>
           
         </fieldset>
       </form>
       </div>
        `
-     );
-   }
-   renderAnswer();
+  );
+  
+  renderChoices();
 }
 
-function renderAnswer() {
-
+function renderChoices() {
+  let q = STORE.quiz[STORE.currentQuestion -1];
+  for(let i=0; i<q.choices.length; i++)
+  {
+    $('.answers').append(`
+        <input type = "radio" name="choices" id="choice${i+1}" value= "${q.choices[i]}" tabindex ="${i+1}" required> 
+        <label for="choice${i+1}"> ${q.choices[i]}</label> <br/>
+        <span id="js-r${i+1}"></span>
+    `);
+  }
 }
 
 
-function submitAnswer() {
-
+function checkAnswer() {
+  $('body').on('submit','#js-questions', function(event) {
+    event.preventDefault();
+  
+    let currentQ = STORE.quiz[STORE.currentQuestion-1];
+    let selectedOption = $('input[name=choices]:checked').val();
+    console.log(selectedOption);
+    //  if (1 === 1) {
+    //    renderCorrect();
+    //  } 
+    //  if (selectedOption === currentQ.correct){
+    //       STORE.score++;
+    //       renderCorrect();
+    //      }
+    //      else{
+    //        renderIncorrect();
+    //      }
+    
+  });
 }
 
-function renderCorrect() {
+// function renderCorrect() {
+//     $('.quiz-area').html(
+//       `
+//       <h2>Congratulations!!!!</h2>
+//       <p class='text'>Keep up the good work</p>
+//       <button class='start'>Next</button>
+//       `
+//     );
+// }
 
-}
+// function renderIncorrect(correct) {
+//   $('.quiz-area').html(
+//     `
+//     <h2>Wrong Answer</h2>
+//     <p class='text'>The correct answer is : <span id='corrected'>${correct}</span></p>
+//     <button class='start'>Next</button>
+//     `
+//   );
 
-function renderIncorrect() {
-
-}
+// }
 
 function renderFinalScore() {
 
@@ -99,7 +120,9 @@ function renderFinalScore() {
 function handleQuizApp(){
 
   loadStartPage();
-  startQuiz();
+  nextQuestion();
+  checkAnswer();
+  
 }
 
 $(handleQuizApp);
