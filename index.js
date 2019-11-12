919c94123502cb5335c43180c1b03e627c700794
Peter Rufo <peter@peterrufo.com>
2019-11-12 12:07:33 -0500

    got questions to work

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 function loadStartPage() {
   $('.quiz-area').html(
     `
-     <p class="intro">How much do you know about weather in the United States?</p>      
+     <p class='intro'>How much do you know about weather in the United States?</p>      
      
-     <div class="button">
-      <button type="button" id="startbutton"> Start Quiz</button>
+     <div>
+      <button type='button' id='startbutton' class='start'> Start Quiz</button>
      </div>
    `
   );
@@ -15,7 +15,8 @@ function loadStartPage() {
 
 
 function nextQuestion() {
-  $('#startbutton').on('click', function(event){
+  // $('#startbutton').on('click', function(event){
+    $('.quiz-area').on('click', '.start', event => {
     event.preventDefault();
     if (STORE.currentQuestion < STORE.quiz.length){
       STORE.currentQuestion++;
@@ -33,8 +34,8 @@ function renderQuestion() {
   $('.quiz-area').html(
     `
       <section class='tickers'>
-      <span class='question-ticker text>Question: ${STORE.currentQuestion}/${STORE.quiz.length}</span>
-      <span class='score-ticker text>Score: ${STORE.score}/${STORE.quiz.length}</span>
+      <span class='question-ticker text'>Question: ${STORE.currentQuestion}/${STORE.quiz.length}</span>
+      <span class='score-ticker text'>Score: ${STORE.score}/${STORE.quiz.length}</span>
        <div>
        <form id="js-questions" class="question-form">
          <fieldset>
@@ -72,12 +73,12 @@ function renderChoices() {
 
 
 function checkAnswer() {
-  $('body').on('submit','#js-questions', function(event) {
-    event.preventDefault();
+  // $('body').on('submit','#js-questions', function(event) {
+  //   event.preventDefault();
   
-    let currentQ = STORE.quiz[STORE.currentQuestion-1];
-    let selectedOption = $('input[name=choices]:checked').val();
-    console.log(selectedOption);
+  //   let currentQ = STORE.quiz[STORE.currentQuestion-1];
+  //   let selectedOption = $('input[name=choices]:checked').val();
+  //   console.log(selectedOption);
     //  if (1 === 1) {
     //    renderCorrect();
     //  } 
@@ -89,32 +90,83 @@ function checkAnswer() {
     //        renderIncorrect();
     //      }
     
+  // });
+  $('.quiz-area').on('submit', event => {
+    event.preventDefault();
+    let userClick = $('input:checked').val();
+    let currentQ = STORE.quiz[STORE.currentQuestion - 1];
+    let correctAnswer = currentQ.correct;
+    if (userClick === correctAnswer){
+      STORE.score++;
+      renderCorrect();
+    }
+    else {
+      renderIncorrect(correctAnswer);
+    }
   });
 }
 
-// function renderCorrect() {
-//     $('.quiz-area').html(
-//       `
-//       <h2>Congratulations!!!!</h2>
-//       <p class='text'>Keep up the good work</p>
-//       <button class='start'>Next</button>
-//       `
-//     );
-// }
+function renderCorrect() {
+    $('.quiz-area').html(
+      `
+      <section class='tickers'>
+      <span class='question-ticker text'>Question: ${STORE.currentQuestion}/${STORE.quiz.length}</span>
+      <span class='score-ticker text'>Score: ${STORE.score}/${STORE.quiz.length}</span>
+      <h2>Congratulations!!!!</h2>
+      <p class='text'>Keep up the good work</p>
+      <button class='start'>Next</button>
+      `
+    );
+}
 
-// function renderIncorrect(correct) {
-//   $('.quiz-area').html(
-//     `
-//     <h2>Wrong Answer</h2>
-//     <p class='text'>The correct answer is : <span id='corrected'>${correct}</span></p>
-//     <button class='start'>Next</button>
-//     `
-//   );
+function renderIncorrect(correct) {
+  $('.quiz-area').html(
+    `
+    <section class='tickers'>
+      <span class='question-ticker text'>Question: ${STORE.currentQuestion}/${STORE.quiz.length}</span>
+      <span class='score-ticker text'>Score: ${STORE.score}/${STORE.quiz.length}</span>    
+    <h2>Wrong Answer</h2>
+    <p class='text'>The correct answer is : <span id='corrected'>${correct}</span></p>
+    <button class='start'>Next</button>
+    `
+  );
 
-// }
+}
 
 function renderFinalScore() {
+  if(STORE.score >= 5){
+    $('.quiz-area').html(
+      `
+      <section class='tickers'>
+        <span class='question-ticker text'>Question: ${STORE.currentQuestion}/${STORE.quiz.length}</span>
+        <span class='score-ticker text'>Score: ${STORE.score}/${STORE.quiz.length}</span>    
+      <h2>Amazing</h2>
+      <p class='text'>You did it</span></p>
+      <button class='restart'>Restart Quiz</button>
+      `
+    );
+  }
+  else {
+    $('.quiz-area').html(
+      `
+      <section class='tickers'>
+        <span class='question-ticker text'>Question: ${STORE.currentQuestion}/${STORE.quiz.length}</span>
+        <span class='score-ticker text'>Score: ${STORE.score}/${STORE.quiz.length}</span>    
+      <h2>Not so good</h2>
+      <p class='text'>try harder</span></p>
+      <button class='restart'>Restart Quiz</button>
+      `
+    );
+  }
+  }
 
+function restartQuiz() {
+  $('quiz-area').on('click', '.restart', event => {
+    event.preventDefault();
+    STORE.currentQuestion = 0;
+    STORE.score = 0;
+    loadStartPage();
+  });
 }
 
 function handleQuizApp(){
@@ -122,6 +174,7 @@ function handleQuizApp(){
   loadStartPage();
   nextQuestion();
   checkAnswer();
+  restartQuiz();
   
 }
 
